f9c1d5f6bc5f4854247892cb9e696f434beb9360
Ali Adnan <aliadnanciit@gmail.com>
2021-08-26 10:42:16 +0400

    add view model paging unit test

--- app/src/test/java/com/shortly/viewmodel/HistoryViewModelTest.kt
+++ app/src/test/java/com/shortly/viewmodel/HistoryViewModelTest.kt
@@ -1,6 +1,8 @@
 package com.shortly.viewmodel
 
+import androidx.paging.PagingData
 import com.shortly.model.datamodel.HistoryModel
+import com.shortly.model.datamodel.state.HistoryPagingViewState
 import com.shortly.model.datamodel.state.HistoryViewState
 import com.shortly.model.usecase.DeleteHistoryUseCase
 import com.shortly.model.usecase.GetHistoryUseCase
@@ -8,6 +10,7 @@ import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert
@@ -15,6 +18,9 @@ import org.junit.Before
 import org.junit.Test
 
 class HistoryViewModelTest {
+
+    @MockK
+    private lateinit var pagingData: PagingData<HistoryModel>
 
     @MockK
     private lateinit var getHistoryUseCase: GetHistoryUseCase
@@ -37,6 +43,16 @@ class HistoryViewModelTest {
 
         Assert.assertEquals(
             HistoryViewState.Success(listOf(getHistoryModel())), historyViewModel.historyStateFlow.value)
+    }
+
+    @Test
+    fun `verify history by paging when history exists`() = runBlockingTest {
+        coEvery { getHistoryUseCase.getPagingHistory() } returns flowOf(pagingData)
+
+        historyViewModel.getPagingHistory()
+
+        Assert.assertEquals(
+            HistoryPagingViewState.Success(pagingData), historyViewModel.historyPagingStateFlow.value)
     }
 
     @Test
